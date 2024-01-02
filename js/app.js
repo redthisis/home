6aa9535584cf79c180f794e6002e77dcbe8aa710
Redthisis <137335446+redthisis@users.noreply.github.com>
2024-01-01 19:14:59 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@
 // let countdownDate = new Date().setMinutes(new Date().getMinutes() + 5)
 
 /* Set countdown date by adding seconds to current date */
-let countdownDate = new Date('30 December 2023 12:00')
+let countdownDate = new Date('5 January 2024 12:00')
 
 let timerInterval
 const daysElem = document.querySelector("#days"),
